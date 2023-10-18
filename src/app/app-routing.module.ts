e68c12af706ca0cdd670d06ebdc1ec47a9b125c6
AdnenKhiari <aymenkhiari484@gmail.com>
2023-10-18 15:17:40 +0100

    multi plage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortfolioPageComponent } from './components/portfolio-page/portfolio-page.component';
+import { TeamsPageComponent } from './components/teams-page/teams-page.component';
+import { AboutUsPageComponent } from './components/about-us-page/about-us-page.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: "portfolio",
+  component: PortfolioPageComponent
+},
+{
+  path: "teams",
+  component: TeamsPageComponent
+},
+
+{
+  path: "about-us",
+  component: AboutUsPageComponent
+},
+{
+  path: '',
+  component: HomePageComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,6 +5,11 @@ import { FooterComponent } from './footer/footer.component';
 import { TeamsComponent } from './teams/teams.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { TeamsPageComponent } from './teams-page/teams-page.component';
+import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
+import { AboutUsPageComponent } from './about-us-page/about-us-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -14,11 +19,16 @@ import { AboutUsComponent } from './about-us/about-us.component';
     FooterComponent,
     TeamsComponent,
     GalleryComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    TeamsPageComponent,
+    PortfolioPageComponent,
+    AboutUsPageComponent,
+    HomePageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
-  exports: [NavBarComponent,FooterComponent,TeamsComponent,GalleryComponent,AboutUsComponent]
+  exports: [AboutUsComponent,PortfolioPageComponent,TeamsPageComponent,NavBarComponent,FooterComponent,TeamsComponent,GalleryComponent,AboutUsComponent]
 })
 export class ComponentsModule { }
